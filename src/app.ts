014b5af2fe19c5a3155b9ea0fef894ac59f9fce5
Nick Wylynko <nick@blueskylabs.com.au>
2021-08-20 11:27:22 +0800

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import Fastify from "fastify";
+import cors from "fastify-cors"
 
 import { indexHandler } from "./endpoints/index"
 
 export const app = Fastify({ logger: true });
+
+
+app.register(cors, { 
+  origin: "*"
+})
 
 app.get("/", indexHandler);
 
