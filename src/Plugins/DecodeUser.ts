84631df562499caebc5e15ec5c1f83879c4879c8
Nick Wylynko <nick@blueskylabs.com.au>
2021-08-20 14:26:08 +0800

    just add the hook directly??

--- src/Plugins/DecodeUser.ts
+++ src/Plugins/DecodeUser.ts
@@ -7,26 +7,4 @@ interface Options {
 
 }
 
-export const DecodeUser = fp((fastify: FastifyInstance, opts: Options, next: any) => {
-
-  fastify.addHook("preHandler", (request: Request, reply, done) => {
-    // add user to request object 
-    fastify.decorateRequest("user", {})
-    const userInfo = decodeUserInfo(request)
-    request.user = userInfo
-    console.log({ userInfo })
-
-    done()
-  })
-  
-  next();
-}, {})
-
-const decodeUserInfo = (request: Request) => {
-  const userInfoBase64 = request.headers["x-apigateway-api-userinfo"] as string
-  const userInfoString = Buffer.from(userInfoBase64, 'base64').toString('ascii')
-  const userInfo = JSON.parse(userInfoString)
-  return userInfo
-}
-
 
--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { Request } from "./Types/Request"
 
 // Plugins
 import cors from "fastify-cors"
-import { DecodeUser } from "./Plugins/DecodeUser"
+// import { DecodeUser } from "./Plugins/DecodeUser"
 
 // Handlers
 import { indexHandler } from "./endpoints/index"
@@ -17,7 +17,25 @@ app.register(cors, {
   origin: "http://localhost:3000"
 })
 
-app.register(DecodeUser, {})
+// app.register(DecodeUser, {})
+
+app.decorateRequest("user", {})
+
+app.addHook("preHandler", (request: Request, reply, done) => {
+  // add user to request object 
+  const userInfo = decodeUserInfo(request)
+  request.user = userInfo
+  console.log({ userInfo })
+
+  done()
+})
+
+const decodeUserInfo = (request: Request) => {
+  const userInfoBase64 = request.headers["x-apigateway-api-userinfo"] as string
+  const userInfoString = Buffer.from(userInfoBase64, 'base64').toString('ascii')
+  const userInfo = JSON.parse(userInfoString)
+  return userInfo
+}
 
 app.get("/", indexHandler);
 
